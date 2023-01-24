ff7402cee6bcb74e8dcc7802c3a6301463975781
AlinaLuninaVyacheslavovna <118982783+AlinaLuninaVyacheslavovna@users.noreply.github.com>
2023-01-24 21:36:40 +0300

    Update task1.py

--- Лабораторная работа№1/task1.py
+++ Лабораторная работа№1/task1.py
@@ -54,6 +54,114 @@ class Wood:
             raise ValueError('Дополнительная нагрузка должна быть положительным числом')
         ...
 
+        
+ class VK:
+    def __int__(self, allowed_number_of_users: int, current_number_of_users: int):
+        """
+        Создание и подготовка к работе объекта "VK"
+
+        :param allowed_number_of_users: Допустимое колчиество пользователей на сайте
+        :param current_number_of_users: Текущее колчиество пользователей на сайте
+
+        Примеры:
+        >>> vk = VK(1500, 500)
+        """
+
+        if not isinstance(allowed_number_of_users, int):
+            raise TypeError('Допустимое колчиество пользователей должно быть типом int')
+        if not allowed_number_of_users > 0:
+            raise ValueError('Допустимое колчиество пользователей должно быть положительным')
+        self.allowed_number_of_users = allowed_number_of_users
+
+        if not isinstance(current_number_of_users, (int, float)):
+            raise TypeError('Текущее колчиество пользователей должно быть типом int')
+        if current_number_of_users < 0:
+            raise ValueError('Текущее колчиество пользователей не может быть отрицательным числом')
+        self.current_number_of_users = current_number_of_users
+
+    def is_not_used(self) -> bool:
+        """
+        Функция проверяет пользуется ли сайтом хоть один человек
+
+        :return: На сайте в данный момент времени никого нет из пользователей
+
+        Примеры:
+        >>> vk = VK(1500, 0)
+        >>> vk.is_not_used()
+        """
+        ...
+
+    def influx_of_users(self, users: int) -> None:
+        """
+        Количство новых пользователей
+
+        :param users: Количество пользователей, зашедших на сайт
+        :raise ValueError: Если колчисество новых и текущих пользователей превышает допустимое, то вызыввем ошибку
+
+        Примеры:
+        >>> vk = VK(1500, 500)
+        >>> vk.influx_of_users(973)
+        """
+        if not isinstance(users, int):
+            raise TypeError('Количество новых пользователей должно быть типом int')
+        if users < 0:
+            raise ValueError('Количество новых пользователей должно быть положительным числом')
+        ...
+
+
+class Table:
+    def __int__(self, number_of_seats: int, number_of_occupied_places: int):
+        """
+        Создание и подготовка к работе объекта "Table"
+
+        :param number_of_seats: Общее число мест за столом
+        :param number_of_occupied_places: Число занятых мест за столом
+
+        Примеры:
+        >>> table = Table(20, 15)
+        """
+
+        if not isinstance(number_of_seats, int):
+            raise TypeError('Общее число мест за столом должно быть типом int')
+        if not number_of_seats > 0:
+            raise ValueError('Общее число мест за столом должно быть положительным')
+        self.number_of_seats = number_of_seats
+
+        if not isinstance(number_of_occupied_places, (int, float)):
+            raise TypeError('Число занятых мест за столом должно быть типом int')
+        if number_of_occupied_places < 0:
+            raise ValueError('Число занятых мест за столом не может быть отрицательным числом')
+        self.number_of_occupied_places = number_of_occupied_places
+
+    def is_not_occupied(self) -> bool:
+        """
+        Функция проверяет сидит ли за столом хоть один человек
+
+        :return: Стол в данный момент времени никем не занят
+
+        Примеры:
+        >>> table = Table(20, 0)
+        >>> table.is_not_occupied()
+        """
+        ...
+
+    def new_occupied_seats(self, seats: int) -> None:
+        """
+        Число новых занятых мест
+
+        :param seats: Число новых занятых мест
+        :raise ValueError: Если сумма новых и ранее занятых мест превышает общее, то вызыввем ошибку
+
+        Примеры:
+        >>> table = Table(20, 15)
+        >>> table.new_occupied_seats(4)
+        """
+        if not isinstance(seats, int):
+            raise TypeError('Число новых занятых мест должно быть типом int')
+        if seats < 0:
+            raise ValueError('Число новых занятых мест должно быть положительным числом')
+        ...
+
 
 if __name__ == "__main__":
     doctest.testmod()
